f9b0dc17dfdd0e02efcb89990130ffc8239db0e3
Ronald Manganaro <ronaldmanganaro@gmail.com>
2016-11-17 13:56:01 -0500

    need to add upload feature

--- app/src/main/java/com/ubiquitious/ronald/finditbuddy/MainActivity.java
+++ app/src/main/java/com/ubiquitious/ronald/finditbuddy/MainActivity.java
@@ -10,6 +10,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -22,10 +25,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         historyBtn = (Button) findViewById(R.id.history_btn);
         findBtn = (Button) findViewById(R.id.find_btn);
         historyBtn.setOnClickListener(myHandler);
         findBtn.setOnClickListener(myHandler);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings, menu);
+        return true;
     }
 
     @Override
@@ -53,55 +64,12 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(historyIntent);
                     break;
                 case R.id.find_btn:
-                    dispatchTakePictureIntent();
+                    Intent findIntent = new Intent(getApplicationContext(), findActivity.class);
+                    startActivity(findIntent);
                     break;
             }
         }
     };
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    private void dispatchTakePictureIntent() {
-
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.CAMERA)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_CONTACTS},
-                        REQUEST_IMAGE_CAPTURE);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        }
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Intent findIntent = new Intent(this, findActivity.class);
-            findIntent.putExtras(extras);
-            startActivity(findIntent);
-        }
-    }
 }
--- app/src/main/java/com/ubiquitious/ronald/finditbuddy/findActivity.java
+++ app/src/main/java/com/ubiquitious/ronald/finditbuddy/findActivity.java
@@ -1,22 +1,31 @@
 package com.ubiquitious.ronald.finditbuddy;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 
 import static android.R.attr.bitmap;
+import static android.R.attr.data;
 import static android.R.attr.format;
 
 /**
@@ -24,34 +33,73 @@ import static android.R.attr.format;
  */
 
 public class findActivity extends AppCompatActivity {
-    protected void onCreate() {
+    ImageView theImage;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    protected void onCreate(Bundle savedInstanceBundle) {
+        super.onCreate(savedInstanceBundle);
         setContentView(R.layout.find_activity);
-        Bundle extras = getIntent().getExtras();
-        if(extras != null){
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            Drawable d = new BitmapDrawable(getResources(), imageBitmap);
+        theImage = (ImageView) findViewById(R.id.thePicture);
+        checkPermissions();
 
-
-
-            ImageView theImage = (ImageView) findViewById(R.id.thePicture);
-
-            theImage.setImageDrawable(d);
-
-        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
-            case R.id.done:
 
-                return true;
-            case R.id.add:
+            case R.id.upload:
 
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.find_menu, menu);
+        return true;
+    }
+
+    public void dispatchTakePictureIntent(View view) {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            theImage.setImageBitmap(imageBitmap);
+
+        }
+    }
+
+    private void checkPermissions() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        REQUEST_IMAGE_CAPTURE);
+
+            }
         }
     }
 }
